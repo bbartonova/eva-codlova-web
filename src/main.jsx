ef45e1f323b4c2d34216d62b965237f41fd2f903
Blanka Bartoňová <bbartonova@gmail.com>
2025-08-18 23:15:11 +0200

    anal vercel add

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { Analytics } from '@vercel/analytics/react'; // ⬅️ přidáno
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics /> {/* ⬅️ přidáno */}
     </BrowserRouter>
   </React.StrictMode>,
 );
